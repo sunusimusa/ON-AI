b22f8ca1430bb83561b46a6bd2da94561c17dceb
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-13 12:28:19 +0100

    server.js

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.get("/register", (req, res) => {
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
-
+app.get("/generator", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "generator.html"));
+});
 // ====== POST APIs ======
 app.post("/register", async (req, res) => {
   // register logic
