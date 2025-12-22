58fd4207d87dccce6d3f46f51faaeb8e9bc8b53b
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-22 21:41:18 +0100

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = "tele-image-ai-v1";
 
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
