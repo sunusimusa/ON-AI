d8f0a7eaff297998d27ee259785b60380c1cc203
sunusimusa <78286160+sunusimusa@users.noreply.github.com>
2025-12-23 23:56:39 +0100

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,3 +3,45 @@ self.addEventListener("install", () => {
 });
 
 self.addEventListener("fetch", () => {});
+const CACHE_NAME = "tele-ai-cache-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
